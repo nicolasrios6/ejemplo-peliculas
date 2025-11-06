47861423be1fb644c2f5fdcda719f6885dac3c4d
Nicolás Ríos <riosnicolas618@gmail.com>
2025-11-06 19:57:10 -0300

    Creo modelos

---
+++ ejemplo-peliculas/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace ejemplo_peliculas.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string ImagenPerfilUrl { get; set; }
+        public List<Favorito> PeliculasFavoritas { get; set; }
+    }
+}
