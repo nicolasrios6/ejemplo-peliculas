ed6fff9589cd8b683f1a2d36ca8938bc03c35e13
Nicolás Ríos <riosnicolas618@gmail.com>
2025-11-10 12:53:59 -0300

    Paginacion y Busqueda en Home

--- ejemplo-peliculas/Controllers/HomeController.cs
+++ ejemplo-peliculas/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace ejemplo_peliculas.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly MovieDbContext _context;
+        private const int PageSize = 8;
 
         public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
@@ -17,9 +18,32 @@ namespace ejemplo_peliculas.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int pagina = 1, string txtBusqueda = "")
         {
-            var peliculas = await _context.Peliculas.ToListAsync();
+            if(pagina < 1) pagina = 1;
+
+            var consulta = _context.Peliculas.AsQueryable();
+            if(!string.IsNullOrEmpty(txtBusqueda))
+            {
+                consulta = consulta.Where(p => p.Titulo.Contains(txtBusqueda));
+            }
+
+
+
+            var totalPeliculas = await consulta.CountAsync();
+            var totalPaginas = (int)Math.Ceiling(totalPeliculas / (double)PageSize);
+
+            if(pagina > totalPaginas && totalPaginas > 0) pagina = totalPaginas;
+
+            var peliculas = await consulta
+                .Skip((pagina - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
+
+            ViewBag.PaginaActual = pagina;
+            ViewBag.TotalPaginas = totalPaginas;
+            ViewBag.TotalPeliculas = totalPeliculas;
+            ViewBag.TxtBusqueda = txtBusqueda;
             return View(peliculas);
         }
 
